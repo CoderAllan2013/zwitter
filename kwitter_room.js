2d1d2005b06851ad1c7c992a0047e787859df11d
Allan Xavier <131976799+CoderAllan2013@users.noreply.github.com>
2024-01-09 18:39:30 +0530

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -29,17 +29,13 @@ getData();
 name = localStorage.getItem("user_name");
 document.getElementById("user_name").innerHTML = "Welcome " + name;
 
-function addRoom() {
-      room_name = document.getElementById("room_name").value;
-
-      firebase.database().ref("/").child(room_name).update({
-            purpose: "adding room name"
-      });
-
-      localStorage.setItem("room_name", room_name);
-
-      window.location = "kwitter_page.html";
+function addRoom() { 
+      room_name = document.getElementById("room_name").value; 
+      firebase.database().ref("/").child(room_name).update({ purpose : "adding room name" }); 
+      localStorage.setItem("room_name", room_name); 
+      window.location = "kwitter_page.html"; 
 }
+
 
 function redirectToRoomName(name) {
       console.log(name);
